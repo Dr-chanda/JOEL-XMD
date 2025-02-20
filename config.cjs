c5df6abeeed85e5945390fc0e93c15063d4f9c93
Dr-chanda <doctorchanda8@gmail.com>
2025-02-20 19:28:56 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -10,7 +10,7 @@ const config = {
   AUTOLIKE_STATUS: process.env.AUTOLIKE_STATUS !== undefined ? process.env.AUTOLIKE_STATUS === 'true' : false,
   AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '💚', // For liking status updates(stories)
   AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS !== undefined ? process.env.AUTO_REPLY_STATUS === 'true' : false,
-  STATUS_READ_MSG: process.env.STATUS_READ_MSG || '`Status viewed by Dr Chanda`',
+  STATUS_READ_MSG: process.env.STATUS_READ_MSG || 'Status viewed by Dr Chanda',
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'false' : false,
   AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
